77aa647eaef2f1180875ea8ebc3bf5d354281ebd
Korobeynik <korobeynik77@gmail.com>
2018-04-03 18:09:25 +0300

    Greated callery

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,51 @@
 $(function() {
 
-	// Mask for phone
-	$("#phone").mask("+380(99) 999-9999");
+	changeImageSrc();
+
+	function changeImageSrc() {
+		$(".product__gallery img").each( function() {
+			let imgSrc = $(this).attr('src');
+			$(this).closest('a').attr("href", imgSrc);
+		});
+	};
+
+  // Mask for phone
+  $("#phone").mask("+380(99) 999-9999");
+
+    //Galerry
+	$(".gallery--init").each(function() {
+		$(this).magnificPopup({
+			delegate: 'a',
+			mainClass: 'mfp-zoom-in',
+			type: 'image',
+			tLoading: '',
+			gallery:{
+				enabled: true,
+			},
+			removalDelay: 300,
+			callbacks: {
+				beforeChange: function() {
+					this.items[0].src = this.items[0].src + '?=' + Math.random();
+				},
+				open: function() {
+					$.magnificPopup.instance.next = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+					}
+					$.magnificPopup.instance.prev = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+					}
+				},
+				imageLoadComplete: function() {
+					var self = this;
+					setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+				}
+			}
+		});
+	});
+
 
 });
